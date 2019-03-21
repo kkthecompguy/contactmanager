0b8dcd817016aa0d0a756d118692bb27b6ab1c68
Kosam Omollo <hnkhosamomollo3@gmail.com>
2019-03-21 23:25:02 +0300

    Updates

---
+++ precache-manifest.27ecc155eac68d8763e99efe4eff384c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3f959bf9ece76ecf367",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "f3f959bf9ece76ecf367",
+    "url": "/contactmanager/static/js/main.f3f959bf.chunk.js"
+  },
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "21fa07d56e05dba0c05e",
+    "url": "/contactmanager/static/css/2.1e554041.chunk.css"
+  },
+  {
+    "revision": "21fa07d56e05dba0c05e",
+    "url": "/contactmanager/static/js/2.21fa07d5.chunk.js"
+  },
+  {
+    "revision": "e209331ba450ac354d9b9dda1dae8cd7",
+    "url": "/contactmanager/index.html"
+  }
+];
